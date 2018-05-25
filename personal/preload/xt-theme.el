ad00c60c8029b06a37690f616d4609e7aeef050e
Rajat Shukla <shk.rajat@gmail.com>
2018-05-25 23:04:19 +0530

    using default emacs theme
    Signed-off-by: Rajat Shukla <shk.rajat@gmail.com>

--- personal/preload/xt-theme.el
+++ personal/preload/xt-theme.el
@@ -1,4 +1,22 @@
+(while custom-enabled-themes
+  (disable-theme (car custom-enabled-themes)))
+
 (disable-theme 'zenburn)
 
-;; (setq prelude-theme nil)
-(setq prelude-theme 'solarized-dark)
+(setq prelude-theme nil)
+
+;; (setq solarized-high-contrast-mode-line t)
+
+;; (setq solarized-distinct-fringe-background t)
+
+;; (setq solarized-scale-org-headlines nil)
+
+;; (setq solarized-height-minus-1 1.0)
+;; (setq solarized-height-plus-1 1.0)
+;; (setq solarized-height-plus-2 1.0)
+;; (setq solarized-height-plus-3 1.0)
+;; (setq solarized-height-plus-4 1.0)
+
+;; (setq x-underline-at-descent-line t)
+
+;; (setq prelude-theme 'solarized-dark)
